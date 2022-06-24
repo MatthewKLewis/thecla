355abf125cd79733dec2e03a9ee0138c5439f3d9
Matthew Lewis <matthew.knight.lewis@gmail.com>
2022-06-23 20:47:51 -0400

    json modeler

--- src/app/people/json-modeler/json-modeler.component.ts
+++ src/app/people/json-modeler/json-modeler.component.ts
@@ -4,17 +4,14 @@ import { Component, Input, OnInit } from '@angular/core';
   selector: 'app-json-modeler',
   template: `
     <div class="wrapper" [style]="'margin-left: ' + level * 10  +'px '">
-
       <div *ngIf="getType(input) == 'array'" style="border: 1px solid purple">
         {{level}}: <span style="font-weight: bold;">{{key}}</span> <span *ngIf="key">:</span> Array
         <app-json-modeler *ngFor="let el of input" [input]="el" [level]="level+1"></app-json-modeler>
       </div>
-
       <div *ngIf="getType(input) == 'object'" style="border: 1px solid red">
         {{level}}: <span style="font-weight: bold;">{{key}}</span>
         <app-json-modeler *ngFor="let el of input | keyvalue" [key]="el.key" [input]="el.value" [level]="level+1"></app-json-modeler>
       </div>
-
       <!--END -->
       <div *ngIf="getType(input) == 'string'" style="border: 1px solid blue; width: 20em">
         {{level}}: <span style="font-weight: bold;">{{key}}</span> <span *ngIf="key">:</span> {{input}} (string)
@@ -25,19 +22,16 @@ import { Component, Input, OnInit } from '@angular/core';
       <div *ngIf="getType(input) == 'boolean'" style="border: 1px solid pink; width: 20em">
         {{level}}: <span style="font-weight: bold;">{{key}}</span> <span *ngIf="key">:</span> {{input}} (boolean)
       </div>
-
       <!--BREAK?-->
       <div *ngIf="getType(input) == 'other'" style="border: 1px solid red">
         {{level}}: <span style="font-weight: bold;">{{key}}</span>
         <app-json-modeler *ngFor="let el of input | keyvalue" [key]="el.key" [input]="el.value" [level]="level+1"></app-json-modeler>
       </div>
-
     </div>
   `,
   styleUrls: ['./json-modeler.component.scss']
 })
 export class JsonModelerComponent implements OnInit {
-
   @Input() input: any;
   @Input() level!: number;
   @Input() key!: any;
@@ -45,7 +39,7 @@ export class JsonModelerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void { }
-
+  
   getType(p: any) {
     if (Array.isArray(p)) return 'array';
     else if (typeof p == 'string') return 'string';
